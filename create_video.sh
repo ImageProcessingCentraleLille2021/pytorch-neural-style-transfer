0d7211f7dcfd1ca245f335db000796829dfe2fbe
Thibault Serti <thibault.serti@gmail.com>
2021-02-12 20:54:52 +0100

    Add cleanup

--- create_video.sh
+++ create_video.sh
@@ -30,11 +30,14 @@ EOF
 
 cleanup() {
   trap - SIGINT SIGTERM ERR EXIT
+  rm "$SCRIPT_DIR"/data/content-images/source/*
+  rm "$SCRIPT_DIR"/data/output-images/*
 }
 
 setup_colors() {
   if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m'
+    BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
   else
     # shellcheck disable=SC2034
     NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
@@ -127,7 +130,8 @@ if [ -n "$end" ]; then
     -to "$end" \
     -s "${height}x${width}" \
     -r "$framerate" \
-    -f image2 "$SCRIPT_DIR/data/content-images/source/image-%3d.png"
+    -f image2 \
+    "$SCRIPT_DIR/data/content-images/source/image-%3d.png"
 else
   ffmpeg -i "$SCRIPT_DIR/videos/video.mp4" \
     -ss "$start" \
@@ -137,7 +141,7 @@ else
     "$SCRIPT_DIR/data/content-images/source/image-%3d.png"
 fi
 
-images=$(find "$SCRIPT_DIR"/data/content-images/source -name "*.png | sort")
+images=$(find "$SCRIPT_DIR"/data/content-images/source -name "*.png" | sort)
 
 for img in $images; do
   python "$SCRIPT_DIR/neural_style_transfer.py" \
@@ -153,3 +157,5 @@ ffmpeg -r "$framerate" \
   -c:v libx264 \
   -y \
   "$SCRIPT_DIR/videos/video-${style%.*}.$format"
+
+cleanup
