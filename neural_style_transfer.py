c04ce6f6631a5966026a8fcb174bc8e482681362
Thibault Ayanides <thibault.serti@gmail.com>
2021-02-22 09:17:35 +0100

    Update readme, clean repo (#13)

--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -177,9 +177,9 @@ if __name__ == "__main__":
     parser.add_argument("--height", type=int, help="height of content and style images", default=400)
 
     parser.add_argument("--content_weight", type=float, help="weight factor for content loss", default=1e5)
-    parser.add_argument("--style_weight", type=float, help="weight factor for style loss", default=3e4)
+    parser.add_argument("--style_weight", type=float, help="weight factor for style loss", default=3e5)
     parser.add_argument("--tv_weight", type=float, help="weight factor for total variation loss", default=1e0)
-    parser.add_argument("--temporal_weight", type=float, help="weight factor temporal loss", default=1e8)
+    parser.add_argument("--temporal_weight", type=float, help="weight factor temporal loss", default=1e9)
 
     parser.add_argument("--optimizer", type=str, choices=['lbfgs', 'adam'], default='lbfgs')
     parser.add_argument("--model", type=str, choices=['vgg16', 'vgg19'], default='vgg19')
